bd03da1162ada1e4b959d6626d2d3b3da3940218
Adall Skoglund <adall.skoglund@gmail.com>
2024-12-05 13:50:30 +0100

    Making the code smaller :)

--- 05/code.py
+++ 05/code.py
@@ -2,8 +2,6 @@ from enum import Enum
 import sys
 from functools import cmp_to_key
 
-rules = {}
-
 class Part(Enum):
     One = 1,
     Two = 2
@@ -17,16 +15,7 @@ def read_rules(file):
         data = file.readline()
     return rules
 
-def compare_to_rules(item1, item2):
-    if item1 in rules and item2 in rules[item1]:
-        return -1
-    if item2 in rules and item1 in rules[item2]:
-        return 1
-    return 0
-
 def main():
-    global rules
-
     # Part 1 or 2. Test or not?
     part = Part.Two if set(["2", "two"]) & set(sys.argv) else Part.One
     test = True if "test" in sys.argv else False
@@ -36,11 +25,14 @@ def main():
     rules = read_rules(file)
     lines = file.readlines()
 
+    # Sort function. Here so we don't have to make rules global
+    comp = lambda i1, i2: -1 if i1 in rules and i2 in rules[i1] else 1 if i2 in rules and i1 in rules[i2] else 0
+
     # CODE
     num = 0
     for line in lines:
         pages = list(map(int, line.strip().split(',')))
-        corrected = sorted(pages, key=cmp_to_key(compare_to_rules))
+        corrected = sorted(pages, key=cmp_to_key(comp))
         if (part == Part.One and corrected == pages) or (part == Part.Two and corrected != pages):
             num += corrected[len(corrected) // 2]
 
