996f3099fb39f184cbad3b712638758dd1274f50
Adall Skoglund <adall.skoglund@gmail.com>
2024-12-05 13:29:23 +0100

    Cleanup and optimizing.

--- 05/code.py
+++ 05/code.py
@@ -12,27 +12,12 @@ class Part(Enum):
 
 def read_rules(file):
     rules = {}
-    while True:
+    data = file.readline()
+    while len(data) > 2:
+        key, value = list(map(int, map(str.strip, data.split("|"))))
+        rules.setdefault(key, [value]).append(value)
         data = file.readline()
-        if len(data) < 2:
-            return rules
-        data = data.split("|")
-        key = int(data[0])
-        value = int(data[1].strip())
-
-        if key not in rules:
-            rules[key] = [value]
-        else:
-            rules[key].append(value)
-
-def update_ok(pages, rules):
-    for i in range(0, len(pages)):
-        page = pages[i]
-        if page in rules:
-            for rule in rules[page]:
-                if rule in pages[0:i]:
-                    return False
-    return True 
+    return rules
 
 def compare_to_rules(item1, item2):
     if item1 in rules and item2 in rules[item1]:
@@ -58,22 +43,11 @@ def main():
     for line in lines:
         pages = list(map(int, line.strip().split(',')))
 
-        if part == Part.One:
-            if update_ok(pages, rules):
-                print(f"OK! {pages} {len(pages)/2} => {pages[ int(len(pages)/2)]}")
-                num += pages[ int(len(pages)/2)]
-        else:
-            if not update_ok(pages, rules):
-                corrected = sorted(pages, key=cmp_to_key(compare_to_rules))
-                #print(f"FIXED! {pages} => {corrected} {len(pages)/2} => {corrected[ int(len(pages)/2)]}")
-                num += corrected[ int(len(corrected)/2)]
-
-#                perms = permutations(pages)
-#                for perm in perms:
-#                    if update_ok(perm, rules):
-#                        print(f"FIXED! {pages} => {perm} {len(pages)/2} => {perm[ int(len(pages)/2)]}")
-#                        num += perm[ int(len(pages)/2)]
-#                        break
+        corrected = sorted(pages, key=cmp_to_key(compare_to_rules))
+        if part == Part.One and corrected == pages:
+            num += corrected[ int(len(corrected)/2)]
+        elif part == Part.Two and corrected != pages:
+            num += corrected[ int(len(corrected)/2)]
 
     # Output
     print("Part: ", part, " Test: ", test)
