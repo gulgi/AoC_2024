ec0fc2457803735ba3951f94c65f381003890713
Adall Skoglund <adall.skoglund@gmail.com>
2024-12-06 10:52:25 +0100

    06, part 2 done

--- 06/code.py
+++ 06/code.py
@@ -22,7 +22,7 @@ def guard_init(data):
                     case 'v':
                         dir = (1, 0)
                 break
-    print(f"Init pos {pos}, dir {dir}")
+    #print(f"Init pos {pos}, dir {dir}")
     return pos, dir
 
 def new_dir(old_dir):
@@ -77,16 +77,46 @@ def main():
                 break
             positions.add(guard_pos)
     else:
-        print("Not done yet :P")
+        loops = 0
+        blocking_positions = set()
+        while True:
+            mod_guard_pos, mod_guard_dir, mod_inside = guard_move(data, guard_pos, guard_dir)
+            if mod_guard_dir == guard_dir and mod_inside and mod_guard_pos not in blocking_positions and mod_guard_pos not in positions:
+                mod_positions = set()
+                mod_data = data.copy()
+                mod_data[mod_guard_pos[0]] = mod_data[mod_guard_pos[0]][:mod_guard_pos[1]] + '#' + mod_data[mod_guard_pos[0]][mod_guard_pos[1]+1:]
+                blocking_pos = mod_guard_pos
+                blocking_positions.add(blocking_pos)
+                mod_guard_pos = guard_pos
+                mod_guard_dir = guard_dir
+                mod_inside = True
+                this_loop = 0
+                while True:
+                    mod_guard_pos, mod_guard_dir_new, mod_inside = guard_move(mod_data, mod_guard_pos, mod_guard_dir)
+                    if not mod_inside:
+                        break
+                    mod_num = len(mod_positions)
+                    mod_positions.add(mod_guard_pos)
+                    if mod_num == len(mod_positions) and mod_guard_dir_new == mod_guard_dir:
+                        this_loop += 1
+                        if this_loop > 1000: # ugly, but to not have this, we need to record direction at each step :P 
+                            #print(f"LOOP !!!  {blocking_pos}")
+                            loops += 1
+                            break
+                    mod_guard_dir = mod_guard_dir_new
 
+            guard_pos, guard_dir, inside = guard_move(data, guard_pos, guard_dir)
+            if not inside:
+                break
+            positions.add(guard_pos)
+        num = loops
+ 
     # Output
     if test:
-        print("Part: ", part, " Test: ", test, " correct: ", num == 41)
+        print("Part: ", part, " Test: ", test, " correct: ", num == 41 if part == Part.One else num == 6)
     else:
         print("Part: ", part, " Test: ", test)
     print("Num: ", num)
 
 if __name__=="__main__":
     main()
-
-# 5305
