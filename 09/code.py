89cf0d48a6bc2f6a3f3bb5c9523d98c99ac29934
Adall Skoglund <adall.skoglund@gmail.com>
2024-12-09 20:53:16 +0100

    09. part 1 works. 2 ... test data works :(

---
+++ 09/code.py
@@ -0,0 +1,129 @@
+from enum import Enum
+import sys
+import string
+
+class Part(Enum):
+    One = 1,
+    Two = 2
+
+def print_memory(blocks):
+    index = 0
+    for block in blocks:
+        for i in range(index, block['pos']):
+            print(f".", end="")
+        for i in range(0, block['blocks']):
+            print(f"{block['id']}", end="")
+        index = int(block['pos']) + int(block['blocks'])
+    print("\n")
+
+def calculate_checksum(blocks):
+    num = 0
+    for block in blocks:
+        for i in range(block['pos'], block['pos']+block['blocks']):
+            num = num + i * int(block['id'])
+    return num
+
+def first_free_memory(blocks, size, before):
+    index = 0
+    previous_block = blocks[0]
+    for block in blocks:
+        free_size = block['pos'] - index 
+        if index < block['pos'] and free_size >= size:
+#            print(f"returning free space: {free_size}")
+            return index, previous_block
+        previous_block = block
+        index = block['pos'] + block['blocks']
+        if before >= 0 and index >= before:
+            break
+    return -1, None
+
+def defrag_one_block(blocks, free_pos, previous_block):
+    last_block = blocks[-1]
+    if last_block['blocks'] == 1:
+        blocks.remove(last_block)
+    else:
+        last_block['blocks'] = last_block['blocks'] - 1
+
+    if previous_block['id'] == last_block['id']:
+        previous_block['blocks'] = previous_block['blocks'] + 1
+    else:
+        # create new block
+        block = {}
+        block['pos'] = free_pos
+        block['id'] = last_block['id']
+        block['blocks'] = 1
+        blocks.insert(blocks.index(previous_block)+1, block)        
+
+def defragment(blocks):
+    while True:
+        free_pos, previous_block = first_free_memory(blocks, 1, -1)
+        if free_pos == -1:
+            break
+        defrag_one_block(blocks, free_pos, previous_block)
+
+def move_blocks(blocks):
+    times = 0
+    for block in reversed(blocks):
+        size = block['blocks']
+        free_pos, previous_block = first_free_memory(blocks, size, block['pos'])
+        if free_pos >= 0:
+#            print(f"Found spot {free_pos}, size {size} for {block['id']}")
+            blocks.remove(block)
+            block['pos'] = free_pos
+            blocks.insert(blocks.index(previous_block)+1, block)
+
+ #       times = times + 1
+ #       if times > 10:
+ #           break
+        #break
+
+def main():
+    # Part 1 or 2. Test or not?
+    part = Part.Two if set(["2", "two"]) & set(sys.argv) else Part.One
+    test = True if "test" in sys.argv else False
+    file_name = "test.txt" if test else "input.txt"
+
+    # Read in file
+    file = open(file_name)
+    data = file.read()
+
+    # massage the data
+    blocks = []
+    size = len(data)
+    index = 0
+    data_index = 0
+    id = 0
+    while index < size:
+        block = {}
+        block['pos'] = data_index
+        block['id'] = id
+        block['blocks'] = int(data[index])
+        blocks.append(block)
+        data_index = data_index + int(data[index])
+        index = index + 1
+        id = id + 1
+        if index >= size:
+            break
+
+        free = int(data[index])
+        data_index = data_index + free
+        index = index + 1
+
+    # CODE
+    print_memory(blocks)
+    if part == Part.One:
+        defragment(blocks)
+    else:
+        move_blocks(blocks)
+    print_memory(blocks)
+
+    num = calculate_checksum(blocks)
+
+    # Output
+    print("Part: ", part, " Test: ", test)
+    print("Num: ", num)
+
+if __name__=="__main__":
+    main()
+
+# 9813645302006 too high
