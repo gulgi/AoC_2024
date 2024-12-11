2637f127542bfee7d3abeae7c1c192707d773cfa
Adall Skoglund <adall.skoglund@gmail.com>
2024-12-11 13:54:47 +0100

    Doing 11 smarter :P

--- 11/code.py
+++ 11/code.py
@@ -1,6 +1,7 @@
 from enum import Enum
 import sys
-#from tqdm import tqdm
+from tqdm import tqdm
+from typing import Iterable, DefaultDict
 
 class Part(Enum):
     One = 1,
@@ -19,27 +20,48 @@ def main():
     # CODE
     num = 0
     times = 75 if part == Part.Two else 25
-    for step in range(0, times):
-#            print(f"{data}")
-        index = 0
-        while True:
-            number = data[index]
-            if number == '0':
-                data[index] = '1'
-                index = index + 1
-            elif len(number) % 2 == 0:
-                split = len(number) // 2
-                data[index] = number[:split]
-                number = str(int(number[split:]))
-                data.insert(index+1, number)
-                index = index + 2
+    """
+        Brute force ....
+        if part == Part.One:
+            for step in tqdm(range(0, times)):
+                index = 0
+                while True:
+                    number = data[index]
+                    if number == '0':
+                        data[index] = '1'
+                        index = index + 1
+                    elif len(number) % 2 == 0:
+                        split = len(number) // 2
+                        data[index] = number[:split]
+                        number = str(int(number[split:]))
+                        data.insert(index+1, number)
+                        index = index + 2
+                    else:
+                        data[index] = str(int(number)*2024)
+                        index = index + 1
+                    if index >= len(data):
+                        break
+            num = len(data)
+        else:
+    """
+    stones = DefaultDict(lambda: 0)
+    for d in data:
+        stones[int(d)] += 1
+
+    for _ in tqdm(range(0, times)):
+        new_stones = DefaultDict(lambda: 0)
+        for stone_number, stone_count in stones.items():
+            stone_number_str = str(stone_number)
+            if stone_number == 0:
+                new_stones[1] += stone_count
+            elif len(stone_number_str) % 2 == 0:
+                split = len(stone_number_str) // 2
+                new_stones[int(stone_number_str[:split])] += stone_count
+                new_stones[int(stone_number_str[split:])] += stone_count
             else:
-                data[index] = str(int(number)*2024)
-                index = index + 1
-            if index >= len(data):
-                break
-#        print(f"{data}")
-    num = len(data)
+                new_stones[stone_number*2024] += stone_count
+        stones = new_stones
+    num = sum(stones.values())
 
     # Output
     print("Part: ", part, " Test: ", test)
