465c676306c020f7e3c813e63f769ae6a6b25de0
Adall Skoglund <adall.skoglund@gmail.com>
2024-12-17 13:06:15 +0100

    17, part 2..? Brute force.

--- 17/code.py
+++ 17/code.py
@@ -32,67 +32,70 @@ def main():
     # CODE
     num = 0
 
-    ip = 0
-    print(f"ip {ip}  ins {program[ip]}  op {program[ip+1]}   A: {A}  B: {B}  C: {C}" )
-    while ip < len(program):
-        time.sleep(0.001)
-        instruction = program[ip]
-        operand = program[ip+1]
+    def combo_rules(combo, A, B, C):
+        if combo >= 0 and combo <= 3:
+            return combo
+        if combo == 4:
+            return A
+        if combo == 5:
+            return B
+        if combo == 6:
+            return C
+        else:
+            print("ERROR  7")
+            exit(1)
+            return -1
 
-#        print(f"ip {ip}  ins {instruction}  op {operand}   A: {A}  B: {B}  C: {C}" )
-        
-        def combo_rules(combo):
-            if combo >= 0 and combo <= 3:
-                return combo
-            if combo == 4:
-                return A
-            if combo == 5:
-                return B
-            if combo == 6:
-                return C
-            else:
-                print("ERROR  7")
-                exit(1)
-                return -1
-            
-        match instruction:
-            case 0: # adv
-                combo_reg = combo_rules(operand)
-                denominator = 2 ** combo_reg
-                A = int(A // denominator)
-                ip += 2
-            case 1: # bxl
-                B = B ^ operand
-                ip += 2
-            case 2: # bst
-                combo_reg = combo_rules(operand)
-                B = combo_reg % 8
-                ip += 2
-            case 3: # jnz
-                if A != 0:
-                    ip = operand
-                else:
-                    ip += 2
-            case 4: # bxc
-                B = B ^ C
-                ip += 2
-            case 5: # out
-                combo_reg = combo_rules(operand)
-#                print(f"OUT {combo_reg % 8}, ")
-                print(f"{combo_reg % 8},", end="")
-                ip += 2
-            case 6: # bdv
-                combo_reg = combo_rules(operand)
-                denominator = 2 ** combo_reg
-                B = int(A // denominator)
-                ip += 2
-            case 7: # cdv
-                combo_reg = combo_rules(operand)
-                denominator = 2 ** combo_reg
-                C = int(A // denominator)
-                ip += 2
-    print("")
-    print(f"ip {ip}  A: {A}  B: {B}  C: {C}" )
+    def run_program(A, B, C):
+        ip = 0
+        program_output = []
+        while ip < len(program):
+            instruction = program[ip]
+            operand = program[ip+1]
+            ip += 2
+            match instruction:
+                case 0: # adv
+                    combo_reg = combo_rules(operand, A, B, C)
+                    denominator = 2 ** combo_reg
+                    A = int(A // denominator)
+                case 1: # bxl
+                    B = B ^ operand
+                case 2: # bst
+                    combo_reg = combo_rules(operand, A, B, C)
+                    B = combo_reg % 8
+                case 3: # jnz
+                    if A != 0:
+                        ip = operand
+                case 4: # bxc
+                    B = B ^ C
+                case 5: # out
+                    combo_reg = combo_rules(operand, A, B, C)
+                    program_output.append(combo_reg % 8)
+                case 6: # bdv
+                    combo_reg = combo_rules(operand, A, B, C)
+                    denominator = 2 ** combo_reg
+                    B = int(A // denominator)
+                case 7: # cdv
+                    combo_reg = combo_rules(operand, A, B, C)
+                    denominator = 2 ** combo_reg
+                    C = int(A // denominator)
+#        print(f"A {A}, B {B}, C {C}")
+        return program_output
+
+    if part == Part.One:
+        program_output = run_program(A, B, C)
+        print(*program_output, sep=",")
+    else:
+        for i in tqdm(range(0, 5_000_0000_000)):
+            A = i
+            B = 0
+            C = 0
+            program_output = run_program(A, B, C)
+            if program_output == program:
+                print(f"Done!  A = {A}")
+                num = A
+                break
+
 
     # Output
     print("Part: ", part, " Test: ", test)
