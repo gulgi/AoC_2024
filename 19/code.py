a80bf7f7f2ed73027ec670765b261d3c9058011f
Adall Skoglund <adall.skoglund@gmail.com>
2024-12-19 10:38:54 +0100

    19. cache helps.

--- 19/code.py
+++ 19/code.py
@@ -1,21 +1,10 @@
 from enum import Enum
 import sys
-from tqdm import tqdm
-from typing import Iterable, DefaultDict
-import re
-import numpy
-from collections import OrderedDict
-import time
-import networkx as nx
-import itertools
+from functools import cache
 
 class Part(Enum):
     One = 1,
     Two = 2
-
-dirs = { 0: numpy.array([-1,0]), 1: numpy.array([0,-1]), 2: numpy.array([1,0]), 3: numpy.array([0,1]) }
-
-width = 6
 
 def main():
     global width
@@ -29,33 +18,27 @@ def main():
     file = open(file_name)
     towels,data = file.read().split("\n\n")
     towels = towels.split(", ")
+    towels.sort(key=lambda s: len(s), reverse=True)
     designs = data.split("\n")
 
-    towels.sort(key=lambda s: len(s), reverse=True)
-    #print(f"towels {towels}")
-    #print(f"designs {designs}")
-    towel_permutations = set(itertools.permutations(towels))
-    print(f"towels {towels}")
-    print(f"towel_permutations {towel_permutations}")
+    @cache
+    def count_designs(design):
+        if len(design) == 0:
+            return 1
+        count = 0
+        for towel in towels:
+            if design.startswith(towel):
+                count += count_designs(design.removeprefix(towel))
+        return count
 
-    def func(design):
-#        G = nx.Graph()
-#        print(f"design: {design} BEFORE")
-        for towel_permutation in towel_permutations:
-            design_try = design
-            for towel in towel_permutation:
-                design_try = design_try.replace(towel, " ")
-#        print(f"design: {design} AFTER")
-            design_try = design_try.strip()
-            if len(design_try) == 0:
-                return True
-        return False
-    
     num = 0
     if part == Part.One:
         for design in designs:
-            if func(design):
+            if count_designs(design) > 0:
                 num += 1
+    else:
+        for design in designs:
+            num += count_designs(design)
 
     # Output
     print("Part: ", part, " Test: ", test)
@@ -65,3 +48,4 @@ if __name__=="__main__":
     main()
 
 # 75 just straight on. Too low, obviusly. Had to try.
+# 97 is wrong ... :/
